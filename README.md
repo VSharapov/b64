d51d3054798ce71193800ffc53e4d5eae51cdf2d
Vasiliy Sharapov <vasiliys@gmail.com>
2020-12-11 16:21:39 +0000

    Handle backspaces

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Toy for base64 encode/decode
 ## TODO
 - [ ] Output something like "<decoded STRING> -> <STRING> -> <encoded STRING>"
 - [ ] Live edit on same line
-    - [ ] Handle backspaces
+    - [x] Handle backspaces
     - [ ] newline starts over
     - [ ] shift+newline starts from copy
--- b64.sh
+++ b64.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 STRING=""
+BACKSPACE="$(echo -n "7f" | xxd -r -p)"
+BACKSPACE=""
 while true; do
   read -s -n1
-  STRING="${STRING}${REPLY}"
-  echo -n $STRING | base64 -w 0
-  echo -n " "
-  echo -n $STRING | xxd -p
+  if [[ "$REPLY" == "$BACKSPACE" ]]; then
+    STRING="${STRING/%?/}"
+  else
+    STRING="${STRING}${REPLY}"
+  fi
+  if [ -z "$STRING" ]; then
+    true
+  else
+    echo -n $STRING | base64 -w 0
+    echo -n " "
+    echo -n $STRING | xxd -p
+  fi
 done
 
